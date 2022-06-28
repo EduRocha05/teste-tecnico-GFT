801ed8648172bfe8bb5ed3d48493bb6b7aa4406b
Eduardo Rocha <edu_rocha05@hotmail.com>
2022-06-28 18:01:46 -0300

    Exercicios

--- exercicio04/src/application/Program.java
+++ exercicio04/src/application/Program.java
@@ -1,4 +1,18 @@
 package application;
 
 public class Program {
+    public static void main(String[] args) {
+        for (int i = 3; i <= 999; i++) {
+            if( Primo(i) )
+                System.out.println(i + " é primo.");
+        }
+    }
+
+    private static boolean Primo(int numero) {
+        for (int j = 2; j < numero; j++) {
+            if (numero % j == 0)
+                return false;
+        }
+        return true;
+    }
 }
